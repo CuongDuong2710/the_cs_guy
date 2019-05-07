b25420427bacc5ba7935496080185c736408a3b8
cuong.duong <cuong.duong@titancorpvn.com>
2019-05-07 17:13:59 +0700

    create tab bar container

--- book_selling_app/lib/details.dart
+++ book_selling_app/lib/details.dart
@@ -76,7 +76,8 @@ class _DetailsPageBodyState extends State<DetailsPageBody> {
               Padding(
                 padding: EdgeInsets.only(top: 43),
                 child: GenreContainer(widget: widget),
-              )
+              ),
+              TabBarContainer(widget: widget)
             ],
           ),
         ],
@@ -307,6 +308,89 @@ class CategoryBox extends StatelessWidget {
             ],
           )
         ],
+      ),
+    );
+  }
+}
+
+class TabBarContainer extends StatelessWidget {
+  final DetailsPageBody widget;
+
+  const TabBarContainer({@required this.widget});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 250,
+      width: double.infinity,
+      child: DefaultTabController(
+        length: 2,
+        child: Scaffold(
+          appBar: PreferredSize(
+            preferredSize: Size.fromHeight(50),
+            child: Container(
+              color: Colors.transparent,
+              child: SafeArea(
+                child: Column(
+                  children: <Widget>[
+                    Expanded(
+                      child: Container(),
+                    ),
+                    TabBar(
+                      labelPadding: EdgeInsets.only(bottom: 15, top: 15),
+                      indicatorColor: Colors.yellow.shade700,
+                      labelColor: Colors.black87,
+                      unselectedLabelColor: Colors.black38,
+                      tabs: <Widget>[Text("Info"), Text("Reviews")],
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+          body: TabBarView(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(20),
+                child: Container(
+                  child: ListView(
+                    children: <Widget>[
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                        child: Text(
+                          "Plot Summary",
+                          style: TextStyle(
+                              fontSize: 28,
+                              fontWeight: FontWeight.w700,
+                              letterSpacing: 1.3),
+                        ),
+                      ),
+                      Padding(
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+                        child: Text(
+                          widget.bookObject.summary,
+                          softWrap: true,
+                          style: TextStyle(fontSize: 15),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+              Center(
+                child: Text(
+                  "NO REVIEWS",
+                  style: TextStyle(
+                      fontWeight: FontWeight.w700,
+                      fontSize: 25,
+                      color: Colors.black54),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
