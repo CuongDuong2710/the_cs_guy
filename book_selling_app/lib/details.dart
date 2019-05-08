b2a5130755ccc3e8f6cc6e7e3a43837ea4076607
cuong.duong <cuong.duong@titancorpvn.com>
2019-05-08 13:49:47 +0700

    add book cover

--- book_selling_app/lib/details.dart
+++ book_selling_app/lib/details.dart
@@ -78,17 +78,36 @@ class _DetailsPageBodyState extends State<DetailsPageBody> {
     return Container(
       child: ListView(
         children: <Widget>[
-          Column(
+          Stack(
             children: <Widget>[
-              TopContainer(widget: widget),
-              AuthorContainer(widget: widget),
-              Padding(
-                padding: EdgeInsets.only(top: 43),
-                child: GenreContainer(widget: widget),
+              Column(
+                children: <Widget>[
+                  TopContainer(widget: widget),
+                  AuthorContainer(widget: widget),
+                  Padding(
+                    padding: EdgeInsets.only(top: 43),
+                    child: GenreContainer(widget: widget),
+                  ),
+                  TabBarContainer(widget: widget)
+                ],
               ),
-              TabBarContainer(widget: widget)
+              Positioned(
+                left: 25,
+                top: 20,
+                child: Container(
+                  height: 260,
+                  width: MediaQuery.of(context).size.width / 2 - 30,
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(0),
+                    child: Image.network(
+                      widget.bookObject.cover,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                ),
+              )
             ],
-          ),
+          )
         ],
       ),
     );
